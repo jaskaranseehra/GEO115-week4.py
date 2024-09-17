dd71357e1c2a413f579c5b02c5fcaa6c26a445a4
jaskaranseehra <jkssira@gmail.com>
2024-09-17 17:43:48 -0400

    first commit

---
+++ lab_task_2.py
@@ -0,0 +1,13 @@
+# Create a dictionary with key-value pairs
+student = {"name": "Jaskaran", "age": 21, "major": "Computer Science"}
+
+# Update a value in the dictionary
+student["age"] = 24
+
+# Access elements by key
+student_name = student["name"]
+
+# Print the dictionary
+print("Student:", student)
+print("Student name:", student_name)
+
